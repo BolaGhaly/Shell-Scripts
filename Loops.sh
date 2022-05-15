710259aefb4494e217b659ef44f773b907e3a353
Bola Ghaly <polaghaly63@gmail.com>
2022-05-15 02:05:50 -0400

    Create Loops.sh

---
+++ Loops.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+#for loop
+for i in 1 2 3 4 5
+do
+  echo "Looping... i = $i"
+done
+
+echo $'\n'
+
+for i in *
+do
+  echo "Looping... i = $i"
+done
+
+
+while [ "$INPUT_STRING" != "bye" ]
+do
+  echo
+  read -p "Type \"bye\" to exit loop: " INPUT_STRING
+  echo "You typed: $INPUT_STRING"
+done
