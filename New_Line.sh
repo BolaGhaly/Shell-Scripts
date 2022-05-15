d94d52a7f7f79c776b4c6fe0d18ead03434d5b74
Bola Ghaly <polaghaly63@gmail.com>
2022-05-15 01:44:18 -0400

    Create New_Line.sh

---
+++ New_Line.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+firstLine="This is the first line"
+secondLine="This is the second line"
+
+# Echo New Line in Bash With -e
+echo -e "$firstLine\n$secondLine\n";
+
+# or .....
+
+# Echo New Line in Bash With $
+echo $firstLine$'\n'$secondLine
