1242342cef524a3f2c8e7ab6558c3e803eea3215
Bola Ghaly <polaghaly63@gmail.com>
2022-05-15 02:16:47 -0400

    Create Arrays.sh

---
+++ Arrays.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+declare -a arr1=( one 2 three 4 .. 100)
+# or arr1=( one two three )
+arr1Len=${#arr1[@]} 
+
+echo
+
+for (( i = 0; i < arr1Len; i++ ));
+do
+  printf "i = $i , arr1[i] = ${arr1[i]}\n"
+done
