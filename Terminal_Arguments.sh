9bdeb31e14616951a2a76a778b47b1c4c039a08d
Bola Ghaly <polaghaly63@gmail.com>
2022-05-15 01:44:25 -0400

    Create Terminal_Arguments.sh

---
+++ Terminal_Arguments.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo -e "\nNo arguments provided\nRun \"./Terminal_Arguments.sh <name> <age>\""
+    exit 1
+elif [ $# -eq 1 ]; then
+    echo -e "\nOnly one argument was provided\nRun \"./Terminal_Arguments.sh <name> <age>\""
+    exit 1
+elif [ $# -gt 2 ]; then
+    echo -e "\nToo many arguments provided\nRun \"./Terminal_Arguments.sh <name> <age>\""
+    exit 1
+elif [ $# -eq 2 ]; then
+    echo -e "\nName: $1\nAge: $2"
+fi
